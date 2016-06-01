77f476426d40c5d04cae51ecee906f517d2ce4a7
Chanho Park <chanho61.park@samsung.com>
2016-06-01 10:38:09 +0900

    artik10: configs: update tzsw partition offset for dfu
    This patch updates tzsw partition offset for dfu according
    to tzsw partition change.
    
    Change-Id: Id61c9e419454898123970a747fd3f4e00eb63dde
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- include/configs/artik10.h
+++ include/configs/artik10.h
@@ -158,15 +158,15 @@
 	"u-boot raw 0x3e 0x800 mmcpart 1;"	\
 	"bl1 raw 0x0 0x1e mmcpart 1;"		\
 	"bl2 raw 0x1e 0x20 mmcpart 1;"		\
-	"tzsw raw 0x83e 0x200 mmcpart 1;"	\
-	"params raw 0xa3f 0x20\0"
+	"tzsw raw 0x83e 0x400 mmcpart 1;"	\
+	"params raw 0x103f 0x20\0"
 
 #define CONFIG_DFU_ALT_BOOT_SD			\
 	"u-boot raw 0x3f 0x800;"		\
 	"bl1 raw 0x1 0x1e;"			\
 	"bl2 raw 0x1f 0x20;"			\
-	"tzsw raw 0x83f 0x200;"			\
-	"params raw 0xa3f 0x20\0"
+	"tzsw raw 0x83f 0x400;"			\
+	"params raw 0x103f 0x20\0"
 
 #define CONFIG_USB_DWC3
 #define CONFIG_USB_DWC3_GADGET
