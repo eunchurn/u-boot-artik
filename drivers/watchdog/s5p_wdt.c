4acac136cc1e3b68c88e4761b7312fc628883967
Heesub Shin <heesub.shin@samsung.com>
2016-12-15 10:44:59 +0900

    s5p: watchdog: introduce h/w watchdog interfaces
    U-boot requires some functions to be provided when we support the
    watchdog. This commit adds them.
    
    Change-Id: Ic4f003e804184d10ad9a08e9ea3df8832249968b
    Signed-off-by: Heesub Shin <heesub.shin@samsung.com>

--- drivers/watchdog/s5p_wdt.c
+++ drivers/watchdog/s5p_wdt.c
@@ -42,3 +42,36 @@ void wdt_start(unsigned int timeout)
 	writel(timeout, &wdt->wtcnt);
 	writel(wtcon, &wdt->wtcon);
 }
+
+static unsigned int wdt_is_started(void)
+{
+	struct s5p_watchdog *wdt =
+		(struct s5p_watchdog *) samsung_get_base_watchdog();
+
+	return !!(readl(&wdt->wtcon) & WTCON_EN);
+}
+
+static void wdt_kick(void)
+{
+	struct s5p_watchdog *wdt =
+		(struct s5p_watchdog *) samsung_get_base_watchdog();
+	unsigned int freq = CONFIG_SYS_CLK_FREQ / (PRESCALER_VAL + 1)
+						/ (16 << WTCON_CLK_128);
+
+	/* kick the dog by reloading wtcnt */
+	writel((CONFIG_HW_WATCHDOG_TIMEOUT_MS * freq) / 1000, &wdt->wtdat);
+	writel((CONFIG_HW_WATCHDOG_TIMEOUT_MS * freq) / 1000, &wdt->wtcnt);
+}
+
+#if defined(CONFIG_HW_WATCHDOG)
+void hw_watchdog_reset(void)
+{
+	if (wdt_is_started())
+		wdt_kick();
+}
+
+void hw_watchdog_init(void)
+{
+	wdt_start(CONFIG_HW_WATCHDOG_TIMEOUT_MS);
+}
+#endif
