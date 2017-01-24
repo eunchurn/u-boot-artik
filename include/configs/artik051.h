19e48647ad7bd38354d9cafff06f622c10adee6f
Heesub Shin <heesub.shin@samsung.com>
2017-01-24 17:14:42 +0900

    artik051: introduces default bootcmd
    Change-Id: I2f24acd41dde5ac2f4e4c57793ae2340c34ae09a
    Signed-off-by: Heesub Shin <heesub.shin@samsung.com>

--- include/configs/artik051.h
+++ include/configs/artik051.h
@@ -54,4 +54,47 @@
 #define CONFIG_HW_WATCHDOG
 #define CONFIG_HW_WATCHDOG_TIMEOUT_MS	10000
 
+#define CONFIG_BOOTCOMMAND	"\n"					\
+	"    if env exists do_rescue; then\n"				\
+	"        c=0;\n"						\
+	"        r=1;\n"						\
+	"        while test ${c} -lt 10 && test ${r} -eq 1; do\n"	\
+	"            run do_checkrescue;\n"				\
+	"            sleep 0.1;\n"					\
+	"            setexpr c ${c} + 1;\n"				\
+	"        done\n"						\
+	"        if test ${r} -eq 1; then\n"				\
+	"            run do_rescue;\n"					\
+	"        fi\n"							\
+	"    fi\n"							\
+	"    run do_boot;"
+
+#define CONFIG_EXTRA_ENV_SETTINGS					\
+	"parta=0x04040000\0"						\
+	"partb=0x04320000\0"						\
+	"bootoffset=0x20\0"						\
+	"dateoffset=0x1ffffc\0"						\
+	"partsize=0x2e0000\0"						\
+	"do_boot=\n"							\
+	"    run do_fwupdate;\n"					\
+	"    setexpr bootaddr ${parta} + ${bootoffset}\n"		\
+	"    echo \"## Verifying image at ${parta} ...\"\n"		\
+	"    sss_auth_img ${parta} && go ${bootaddr}\n"			\
+	"    echo \"## Trying to recover ...\"\n"			\
+	"    if sss_auth_img ${partb}; then\n"				\
+	"        erase ${parta} +${partsize}\n"				\
+	"        cp.b ${partb} ${parta} ${partsize}\n"			\
+	"    fi\n"							\
+	"    reset\0"							\
+	"do_fwupdate=\n"						\
+	"    setexpr parta_date ${parta} + ${dateoffset}\n"		\
+	"    setexpr partb_date ${partb} + ${dateoffset}\n"		\
+	"    if itest *${parta_date} -lt *${partb_date}; then\n"	\
+	"        if sss_auth_img ${partb}; then\n"			\
+	"            echo \"## Updating firmware ...\"\n"		\
+	"            erase ${parta} +${partsize}\n"			\
+	"            cp.b ${partb} ${parta} ${partsize}\n" 		\
+	"        fi\n"							\
+	"    fi\0"
+
 #endif	/* __CONFIG_ARTIK_051_H */
