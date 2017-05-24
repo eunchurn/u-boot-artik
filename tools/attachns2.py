2813f9bbc6d8860dbd244582efe6807bf09fa2c9
Heesub Shin <heesub.shin@samsung.com>
2017-05-24 13:53:35 +0900

    artik05x: add attachns2.py
    The first stage bootloader of ARTIK-05x expects that the subsequent
    binary image have binary header which contains size information and
    simple SHA256 checksum at offset zero. attachns2.py creates a valid
    binary image from the raw binary.
    
    Change-Id: Ib66f95788c46ae457b0e9f6a119c9e38ea70c75f
    Signed-off-by: Heesub Shin <heesub.shin@samsung.com>

---
+++ tools/attachns2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+import os
+import sys
+import hashlib
+import struct
+
+try:
+    inputfile = sys.argv[1]
+    outputfile = sys.argv[2]
+except IndexError:
+    print "Usage: attachns2.py <input> <output>"
+    print "Example:"
+    print "   attachns2.py ./u-boot.bin ./u-boot.head.bin"
+    sys.exit(1)
+
+fin = open(inputfile, 'rb')
+fout = open(outputfile, 'wb+')
+
+h = hashlib.sha256();
+
+data = fin.read()
+size = fin.tell()
+
+# prepare output file
+size = (size + 32 + 1024) & ~(512 - 1)
+fout.truncate(size)
+
+# write binary image
+fout.seek(32, 0)
+fout.write(data)
+
+# calc sha256
+fout.seek(32, 0)
+data = fout.read(size - 32 - 272)
+h = hashlib.sha256(data)
+
+# put headers
+fout.seek(0, 0)
+fout.write(struct.pack('III', size / 512, 0x0, 0x656d6264))
+fout.seek(4, 0)
+digest = h.digest()[:4]
+fout.write(digest)
+
+fin.close()
+fout.close()
